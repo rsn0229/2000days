87382b755cf29f536cd1d71a833bfb2a1e611a88
rsn0229 <rsn0229@gmail.com>
2026-03-22 20:19:28 +0900

    Update api.js

--- api.js
+++ api.js
@@ -1,11 +1,11 @@
 const GAS_URL = "https://script.google.com/macros/s/AKfycbyPRKkR0kKwC9x7msErQDJLU6yu0BleU1F9ySRBwC6RGlSVmzQJrcUGZ_4-Z45pXYX60A/exec";
 
 async function saveNickname(nickname) {
-  // 몰래 백그라운드에서 실행되도록 await를 강제하지 않음
+  
   fetch(GAS_URL, {
     method: "POST",
-    mode: "no-cors", // CORS 우회용. 응답을 읽지 않아도 전송은 됨
-    // 💡 [수정] application/json 대신 text/plain을 써야 브라우저가 보안 검열(Preflight) 없이 바로 데이터를 쏴줍니다!
+    mode: "no-cors", 
+
     headers: { "Content-Type": "text/plain" }, 
     body: JSON.stringify({ action: "save", name: nickname })
   }).catch(err => console.error("백그라운드 저장 실패", err));
